dff0a6f4b91fd668da13b230f872431fc1a83f64
seriouscat96 <thisisbusiness@yandex.ru>
2021-04-04 22:26:47 +0300

    fixed cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -26,11 +26,11 @@ mongoose.connect(NODE_ENV === 'production' ? DB_CONNECTION_STRING : 'mongodb://l
 });
 
 const options = {
-  origin: [
-    'http://localhost:3000',
-    'http://mesto.nomoredomains.icu',
-    'https://seriouscat96.github.io',
-  ],
+  origin: 'http://mesto.nomoredomains.icu',
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  allowedHeaders: ['Content-Type', 'origin'],
   credentials: true,
 };
 app.use('*', cors(options));
@@ -51,11 +51,11 @@ app.use(authHandler);
 
 app.use('/cards', cards);
 app.use('/users', users);
-app.all('/*', notFoundHandler);
 
 app.use(errorLogger);
 app.use(errors());
 
+app.use(notFoundHandler);
 app.use(errorHandler);
 
 app.listen(PORT, () => {
